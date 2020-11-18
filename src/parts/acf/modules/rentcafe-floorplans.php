fcaab30d78fd9ea518055599c2e03435992c0ea8
Al Notara <al@vallgroup.com>
2020-11-18 21:05:05 +1100

    included site map in rentcafe shortcode implementation

--- src/parts/acf/modules/rentcafe-floorplans.php
+++ src/parts/acf/modules/rentcafe-floorplans.php
@@ -2,10 +2,14 @@
 /**
  * Template file for displaying a Rentcafe Floorplans
  */
+
+$site_map = get_field( 'site_map', 'options' )
+  ? get_field( 'site_map', 'options' )
+  : null;
 ?>
 
 <section class="tq-rentcafe-floorplans">
   <div class="content-wrapper">
-    <?php echo do_shortcode( '[torque_rentcafe]' ); ?>
+    <?php echo do_shortcode( '[torque_rentcafe site_map="' . $site_map . '"]' ); ?>
   </div>
 </section>
